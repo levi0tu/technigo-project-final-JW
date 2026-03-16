859f9fdcd481cb15cccc43c3e054b99b1c4e9961
Jeanette Wiktorsson <leiia@hotmail.com>
2026-03-16 19:06:48 +0100

    Set up auth context for protected routes

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,7 +1,9 @@
+import { useContext } from "react"
 import { Navigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 export const ProtectedRoute = ({ children }) => {
-    const isLoggedIn = false
+    const { isLoggedIn } = useContext(AuthContext)
 
     if (!isLoggedIn) {
         return <Navigate to="/login" />
