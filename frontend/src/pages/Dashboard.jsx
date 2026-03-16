cd6d5f1da900c2a0c578d22afba8df40fd98af8b
Jeanette Wiktorsson <leiia@hotmail.com>
2026-03-16 19:40:59 +0100

    Add basic login and logout flow

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,20 @@
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
+
 export const Dashboard = () => {
-    return <h2>Dashboard</h2>
+    const { setIsLoggedIn } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        setIsLoggedIn(false)
+        navigate("/login")
+    }
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <button onClick={handleLogout}>Log out</button>
+        </div>
+    )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,21 @@
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+
+
 export const Login = () => {
-    return <h2>Login</h2>
+    const { setIsLoggedIn } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+        navigate("/dashboard")
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    )
 }
