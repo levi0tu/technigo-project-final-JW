708af438011685d0beef1e864d8cd70dd9cb6bbf
Jeanette Wiktorsson <leiia@hotmail.com>
2026-03-17 13:36:35 +0100

    Build basic controlled login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Layout } from "../components/Layout"
 import { AuthContext } from "../context/AuthContext"
@@ -8,7 +8,12 @@ export const Login = () => {
     const { setIsLoggedIn } = useContext(AuthContext)
     const navigate = useNavigate()
 
-    const handleLogin = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+    const handleSubmit = (event) => {
+        event.preventDefault()
         setIsLoggedIn(true)
         navigate("/dashboard")
     }
@@ -17,7 +22,27 @@ export const Login = () => {
         <Layout>
             <h2>Logga in</h2>
             <p>Växla upp din koll på pengarna.</p>
-            <button onClick={handleLogin}>Logga in</button>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">E-post</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={(event) =>
+                        setFormData({ ...formData, email: event.target.value })}
+                    required />
+                <label htmlFor="password">Lösenord</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={(event) =>
+                        setFormData({ ...formData, password: event.target.value })}
+                    required />
+                <button>Logga in</button>
+            </form>
         </Layout>
     )
 }
