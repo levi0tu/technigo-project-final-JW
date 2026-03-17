c7a73064ba3c273442b420e97a35778c39d45340
Jeanette Wiktorsson <leiia@hotmail.com>
2026-03-17 09:12:12 +0100

    Add debt list with dynamic detail routes

--- frontend/src/pages/DebtDetail.jsx
+++ frontend/src/pages/DebtDetail.jsx
@@ -1,10 +1,13 @@
+import { useParams } from "react-router-dom"
 import { Layout } from "../components/Layout"
 
 export const DebtDetail = () => {
+    const { id } = useParams()
+
     return (
         <Layout>
             <h2>Debt Detail</h2>
-
+            <p>Debt ID: {id}</p>
         </Layout>
     )
 }
--- frontend/src/pages/Debts.jsx
+++ frontend/src/pages/Debts.jsx
@@ -2,10 +2,19 @@ import { Link } from "react-router-dom"
 import { Layout } from "../components/Layout"
 
 export const Debts = () => {
+    const debts = [
+        { id: 1, name: "Phone" },
+        { id: 2, name: "Jeans" },
+    ]
+
     return (
         <Layout>
             <h2>My Debts</h2>
-            <Link to="/debts/1">Open Debt</Link>
+            {debts.map((debt => (
+                <div key={debt.id}>
+                    <Link to={`/debts/${debt.id}`}>{debt.name}</Link>
+                </div>
+            )))}
         </Layout>
     )
 }
