0eddfd1ef3a3c47108d91a69e46348308d8a6945
Jeanette Wiktorsson <leiia@hotmail.com>
2026-04-04 18:23:01 +0200

    Prepare frontend for deployment with env config

--- frontend/src/services/debtService.js
+++ frontend/src/services/debtService.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = import.meta.env.VITE_API_URL
+
 
 
 export const getDebts = async () => {
--- frontend/src/services/lessonService.js
+++ frontend/src/services/lessonService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export const getLessons = async () => {
     const response = await fetch(`${BASE_URL}/lessons`)
